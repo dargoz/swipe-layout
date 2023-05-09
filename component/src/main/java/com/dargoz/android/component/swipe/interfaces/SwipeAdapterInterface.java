9530398a3b36d1cdb817671258ddc66f5843d23d
Davin Reinaldo Gozali <davin_gozali@bca.co.id>
2023-05-09 15:37:47 +0700

    initial source

---
+++ component/src/main/java/com/dargoz/android/component/swipe/interfaces/SwipeAdapterInterface.java
@@ -0,0 +1,9 @@
+package com.dargoz.android.component.swipe.interfaces;
+
+public interface SwipeAdapterInterface {
+
+    int getSwipeLayoutResourceId(int position);
+
+    void notifyDatasetChanged();
+
+}
---
+++ component/src/main/java/com/dargoz/android/component/swipe/util/Attributes.java
@@ -0,0 +1,9 @@
+package com.dargoz.android.component.swipe.util;
+
+
+public class Attributes {
+
+    public enum Mode {
+        Single, Multiple
+    }
+}
